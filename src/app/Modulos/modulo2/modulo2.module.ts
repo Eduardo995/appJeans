b8af14609eb1c85a5915f90191518c848a054c8a
Anthony-ui <morante2302@gmail.com>
2021-02-22 23:15:39 -0500

    final

--- src/app/Modulos/modulo2/modulo2.module.ts
+++ src/app/Modulos/modulo2/modulo2.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from 'src/app/Vistas/menu/menu.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [MenuComponent],
+
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
   ],
-  exports:[
-    MenuComponent
-  ]
+  exports: [ MenuComponent]
+
 })
 export class Modulo2Module { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { FormsModule } from '@angular/forms';
 import { Modulo2Module } from './Modulos/modulo2/modulo2.module';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy, RouterModule } from '@angular/router';
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http'
 
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), FormsModule,AppRoutingModule,RouterModule,Modulo2Module,HttpClientModule,Modulo2Module],
+  imports: [BrowserModule, IonicModule.forRoot(), FormsModule,AppRoutingModule,RouterModule,HttpClientModule,Modulo2Module],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+
 })
 export class AppModule {}
 
